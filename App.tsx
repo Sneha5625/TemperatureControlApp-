ecff40b94ed0a1e03fec17ce9da478447fc407f9
Sneha5625 <117165070+Sneha5625@users.noreply.github.com>
2023-01-24 21:19:47 +0530

    1 file modified

--- App.tsx
+++ App.tsx
@@ -1,100 +1,111 @@
 import * as React from 'react';
 import './style.css';
 import { useStyletron } from 'baseui';
-import { styled } from 'baseui';
+import { Button, SHAPE, SIZE } from 'baseui/button';
 
-const Button = styled('button', ({ $theme }) => {
-  return {
-    padding: '10px',
-    width: '5vw',
-    height: '10vh',
-    borderRadius: '50%',
-    fontSize: '35px',
-    background: 'rgba(255, 112, 0, 0.92)',
-    border: $theme.borders.border600,
-    color: 'white',
-    margin: '10px 10% 5% 13%',
-    '@media screen and (max-width:600px)': {
-      width: '15vw',
-      padding: '5px',
-      fontSize: '20px',
-    },
-  };
-});
 export default function App() {
   const [value, setValue] = React.useState(10);
-  const [color, setColor] = React.useState(false);
+  const [color, setColor] = React.useState(true);
   const [css, theme] = useStyletron();
+
   function handleIncrement() {
     const temp = value + 1;
-    if (temp >= 20) {
-      setColor(true);
+    if (temp > 35) {
+      return;
     }
-
-    setValue(temp);
-  }
-  function handleDecrement() {
-    const temp = value - 1;
-    if (temp < 20) {
+    if (temp >= 17) {
       setColor(false);
     }
-
     setValue(temp);
   }
 
+  function handleDecrement() {
+    const temp = value - 1;
+    if (temp < 0) {
+      return;
+    }
+    if (temp < 15) {
+      setColor(true);
+    }
+    setValue(temp);
+  }
   return (
     <div
       className={css({
-        background: theme.colors.primary500,
-        margin: '10% 40%',
-        width: '20vw',
-        height: '55vh',
-        border: '1px solid rgba(17, 17, 22, 0.76)',
-        borderRadius: '30px',
-
-        '@media screen and (max-width:600px)': {
-          width: '60vw',
-          margin: '30% 20% ',
-        },
+        background: '#9CC3D5FF',
+        padding: '4rem',
+        display: 'flex',
+        flexDirection: 'column',
+        flexWrap: 'wrap',
+        justifyContent: 'center',
+        position: 'absolute',
+        top: '50%',
+        left: '50%',
+        borderRadius: '25px',
+        transform: 'translate(-50%, -50%)',
       })}
     >
       <div
         className={css({
-          background: color ? theme.colors.warning : 'rgba(0, 212, 255, 0.91)',
-          color: 'white',
-          margin: '20% ',
-          width: '10vw',
-          height: '20Vh',
-          padding: '10px',
-          border: '2px solid white',
+          background: color ? '#0063B2FF' : 'orange',
+          height: '12rem',
+          width: '12rem',
           borderRadius: '50%',
-          textAlign: 'center',
-          '@media screen and (max-width:600px)': {
-            width: '25vw',
-            height: '15vh',
-            margin: '25%',
-          },
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+          fontSize: '25px',
+          color: 'white',
+          border: '2px solid white',
         })}
       >
-        <p
-          className={css({
-            textAlign: 'center',
-            marginTop: '40%',
-            fontSize: '40px',
-            '@media screen and (max-width:600px)': {
-              fontSize: '20px',
-              margin: '28%',
-            },
-          })}
-        >
-          {value}
-          <sup>0</sup>C
-          <br />
-        </p>
+        {value}°C
       </div>
-      <div>
-        <Button onClick={handleIncrement}>+</Button>
-        <Button onClick={handleDecrement}>-</Button>
+      <div
+        className={css({
+          display: 'flex',
+          justifyContent: 'space-around',
+          paddingTop: '30px',
+        })}
+      >
+        <Button
+          overrides={{
+            BaseButton: {
+              style: {
+                backgroundColor: '#0063B2FF',
+                fontSize: '20px',
+                border: '2.5px solid white',
+                ':hover': {
+                  backgroundColor: '#0063B2FF',
+                },
+              },
+            },
+          }}
+          size={SIZE.large}
+          shape={SHAPE.circle}
+          onClick={handleIncrement}
+        >
+          +
+        </Button>
+        <Button
+          overrides={{
+            BaseButton: {
+              style: {
+                backgroundColor: '#0063B2FF',
+                fontSize: '20px',
+                border: '2.5px solid white',
+                ':hover': {
+                  backgroundColor: '#0063B2FF',
+                },
+              },
+            },
+          }}
+          size={SIZE.large}
+          shape={SHAPE.circle}
+          onClick={handleDecrement}
+        >
+          -
+        </Button>
       </div>
     </div>
   );
